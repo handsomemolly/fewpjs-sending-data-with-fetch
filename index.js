33b39e1020c85de51eb5c3d729b0b1db64c98180
handsomemolly <ryan.kendig@gmail.com>
2021-01-14 11:12:11 -0800

    finished

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData() {
+    return fetch('http://localhost:3000/users', configurationObject).then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id
+    })
+    .catch(function(error) {
+        alert("Whoopsy Daisy")
+        document.body.innerHTML = error.message
+    })
+}
+
+let configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com"
+    })
+}
